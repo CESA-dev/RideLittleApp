be71f414e639255ff65b621df05ef19941aa2062
Jiarui Sun <jiaruisun@wirelessprvnat-172-16-198-101.near.illinois.edu>
2018-01-28 12:48:32 -0600

    history a bit

--- pages/history/history.js
+++ pages/history/history.js
@@ -1,66 +1,39 @@
-// pages/history/history.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    textArray: [{ route: 'ORD-香槟', date: '12/18/2017', time: '1 AM', car: 'BMW X5' }, { route: '香槟-ORD', date: '12/18/2017', time: '1 AM', car: 'BMW X5' }, { route: 'ORD-香槟', date: '12/18/2017', time: '1 AM', car: 'BMW X5' }, { route: '香槟-普渡', date: '12/18/2017', time: '1 AM', car: 'BMW X5' }, { route: '普渡-香槟', date: '12/18/2017', time: '1 AM', car: 'BMW X5' }, { route: '香槟-ORD' },]
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
   
+  toDetailView: function (e) {
+    console.log(e)
+    let title = e.currentTarget.dataset.title
+    let date = e.currentTarget.dataset.date
+    let time = e.currentTarget.dataset.time
+    let car = e.currentTarget.dataset.car
+    wx.navigateTo({
+      url: '../detail/detail?title=' + title + "&date="
+      + date + "&time=" + time + "&car=" + car
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  toNewRideView: function (e) {
+    wx.navigateTo({
+      url: '../newRide/newRide'
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  bindKeyInput: function (e) {
+    console.log(e.detail.value)
   }
+
+
 })

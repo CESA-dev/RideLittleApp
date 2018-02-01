a6c43cc5344aacab59c97e9f5205e5c6bc0facf0
Deanplayerljx <deanplayerljx@gmail.com>
2018-01-31 23:09:29 -0600

    search ui and locations

--- pages/search/search.js
+++ pages/search/search.js
@@ -4,8 +4,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    start: [{name: 'ORD', selected: false }, {name: '芝加哥市区', selected: false }, {name: '香槟', selected: false }, {name: '其他', selected: false }],
+    destination: [{ name: 'ORD', selected: false }, { name: '芝加哥市区', selected: false }, { name: '香槟', selected: false }, { name: '其他', selected: false }]
   },
+   
 
   /**
    * 生命周期函数--监听页面加载
@@ -77,4 +79,41 @@ Page({
       time: e.detail.value
     })
   },
+
+  addLoc: function (e) {
+    console.log(e);
+    let id = e.currentTarget.id;
+    let isStart = id.substring(0,3) === 'des' ? false : true;
+
+    let locs = [];
+    if (isStart) {
+      locs = this.data.start;
+    }
+    else {
+      locs = this.data.destination;
+    }
+    let idx = id[id.length-1];
+    // if the seat clicked is empty
+    if (locs[idx].selected === false) {
+      // parse the locs to remove the old loc selected
+      locs.forEach((val, idx, arr_whole) => {
+        if (val.selected) {
+          val.selected = false;
+        }
+      });
+      // add user info
+      locs[idx].selected = true;
+    }
+    if (isStart) {
+      this.setData({
+        start: locs
+      });
+    }
+    else {
+      this.setData({
+        destination: locs
+      });
+    }
+    
+  }
 })

fe4bbf1a9128bf577f1e73e9a043d821fa7212d4
tonylitianyu <tonylitianyu@gmail.com>
2018-02-18 15:56:34 -0600

    创建新ride页面

--- pages/newRide/newRide.js
+++ pages/newRide/newRide.js
@@ -4,8 +4,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    start: [{ name: 'ORD', selected: false }, { name: '芝加哥市区', selected: false }, { name: '香槟', selected: false }, { name: '其他', selected: false }],
+    destination: [{ name: 'ORD', selected: false }, { name: '芝加哥市区', selected: false }, { name: '香槟', selected: false }, { name: '其他', selected: false }],
+    numberOfSeat: [1,2,3,4,5],
+    selectDate:"",
+    selectTime:"",
+    selectSeatNumber:0,
   },
+
 
   /**
    * 生命周期函数--监听页面加载
@@ -61,5 +67,73 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      
+      date: e.detail.value,
+      selectDate: e.detail.value
+    })
+  },
+
+  bindTimeChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      time: e.detail.value,
+      selectTime: e.detail.value
+    })
+  },
+
+  bindSeatChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value,
+      selectSeatNumber: e.detail.value
+    })
+  },
+
+  addLoc: function (e) {
+    console.log(e);
+    let id = e.currentTarget.id;
+    let isStart = id.substring(0, 3) === 'des' ? false : true;
+
+    let locs = [];
+    if (isStart) {
+      locs = this.data.start;
+    }
+    else {
+      locs = this.data.destination;
+    }
+    let idx = id[id.length - 1];
+    // if the seat clicked is empty
+    if (locs[idx].selected === false) {
+      // parse the locs to remove the old loc selected
+      locs.forEach((val, idx, arr_whole) => {
+        if (val.selected) {
+          val.selected = false;
+        }
+      });
+      // add user info
+      locs[idx].selected = true;
+    }
+    if (isStart) {
+      this.setData({
+        start: locs
+      });
+    }
+    else {
+      this.setData({
+        destination: locs
+      });
+    }
+
+  },
+
+  create:function(e){
+    console.log('创建房间' + this.data.selectSeatNumber + this.data.selectDate + this.data.selectTime);
+    
   }
 })
